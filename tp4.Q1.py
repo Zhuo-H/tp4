7b900090e5572bb2d262b9cf3bf64b215eb56924
hanz <hanz@educ.college-montreal.qc.ca>
2025-10-29 10:32:46 -0400

    stuff

---
+++ tp4.Q1.py
@@ -0,0 +1,9 @@
+class StringFoo:
+    def __init__(self, message):
+        self.message = message
+    def printstring(self):
+        print(self.message.upper())
+
+m1 = StringFoo("i like apples")
+
+m1.printstring()
