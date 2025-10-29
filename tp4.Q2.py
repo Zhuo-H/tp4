868ec0e8f32e1ee272ee2f9eecdc50335a313e68
hanz <hanz@educ.college-montreal.qc.ca>
2025-10-29 10:44:13 -0400

    stuff

--- tp4.Q2.py
+++ tp4.Q2.py
@@ -1,9 +1,13 @@
 class rectangle:
-    def __init__(self,x, y):
-        self.x == x
-        self.y == y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
     def calcul_aire(self):
         print(self.x * self.y)
     def info(self):
         print(f"la longuer est{self.x} et la largeur est {self.y}")
 
+
+rectangle = rectangle(10,4)
+rectangle.calcul_aire()
+rectangle.info()
