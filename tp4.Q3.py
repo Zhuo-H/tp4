ca128e1fc518d0132b83427e18864219efa4ed31
hanz <hanz@educ.college-montreal.qc.ca>
2025-10-30 21:39:01 -0400

    finished

---
+++ tp4.Q3.py
@@ -0,0 +1,18 @@
+import math
+from dataclasses import dataclass, field
+
+@dataclass
+class Circle:
+    rayon: int
+
+    def aire(self):
+        a_result = math.pi * self.rayon ** 2
+        print(f"Le circonference est {round(a_result, 2)}")
+    def circonference(self):
+        c_result = 2 * math.pi * self.rayon
+        print(f"Le circonference est {round(c_result, 2)}")
+
+
+c1 = Circle(10)
+c1.aire()
+c1.circonference()
