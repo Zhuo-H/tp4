af217f4693a6811dc05a586d43a565d48bc2cfa6
hanz <hanz@educ.college-montreal.qc.ca>
2025-11-13 15:30:13 -0500

    minor changes

--- tp4.Q2.py
+++ tp4.Q2.py
@@ -3,12 +3,14 @@ class Rectangle:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.aire = 0
 
     def calcul_aire(self):
-        print(self.x * self.y)
+        self.aire = self.x * self.y
+        return self.aire
 
     def info(self):
-        print(f"la longuer est{self.x} et la largeur est {self.y}")
+        print(f"la longuer est {self.x}, la largeur est {self.y} et l'aire est {self.aire}")
 
 
 rectangle = Rectangle(20, 4)
--- tp4.Q4.py
+++ tp4.Q4.py
@@ -1,17 +1,15 @@
 from dataclasses import dataclass, field
 import random
-from tkinter.font import names
+
 
 
 def dice(max):
     result = random.randint(1, max)
     return result
+
+
 @dataclass
-class Hero:
-    name: str
-    hp: int = dice(10) + dice(10)
-    attaque: int = dice(6)
-    defence: int = dice(6)
+class NPCStats:
     force: int = dice(20)
     dexterity: int = dice(20)
     intelligence: int = dice(20)
@@ -19,21 +17,41 @@ class Hero:
     constitution: int = dice(20)
     wisdom: int = dice(20)
 
+
+class Hero:
+    def __init__(self, nom):
+        self.stats = NPCStats()
+        self.name = nom
+        # hp: int = dice(10) + dice(10)
+        # attaque: int = dice(6)
+        # defence: int = dice(6)
+
+    def print_stats(self):
+        print(self.name)
+        print(self.stats.force)
+
     def faire_attaque(self):
         damage = self.attaque + dice(6)
         print(damage)
+
     def take_damage(self, t_damage):
         self.hp -= t_damage
         print(self.hp)
+
     def alive(self):
         if self.hp <= 0:
             print("you are dead")
             return False
 
 
-h1 = Hero("Gerald")
-h1.faire_attaque()
-h1.take_damage(10)
-h1.alive()
+h1 = Hero('test1')
+h2 = Hero('test21')
 
+h1.print_stats()
+h2.print_stats()
+
+# h1.faire_attaque()
+# h1.take_damage(10)
+# h1.alive()
+#
 
