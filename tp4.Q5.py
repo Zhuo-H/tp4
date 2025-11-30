88d5e3be486aa307dc51fbca1a469144b74930c6
hanz <hanz@educ.college-montreal.qc.ca>
2025-11-30 17:16:14 -0500

    main things finished

--- tp4.Q5.py
+++ tp4.Q5.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+#from dataclasses import dataclass
 import random
 
 
@@ -13,8 +13,8 @@ def picker():
     return sum(rolls_sorted[:3])
 
 
-class statistics:
-    def __init__(self):
+class NPC:
+    def __init__(self, name):
         self.armor = dice(12)
         self.hp = dice(20)
         self.force = picker()
@@ -23,9 +23,34 @@ class statistics:
         self.intelligence = picker()
         self.charisma = picker()
         self.sagesse = picker()
-
+        self.name = name
     def stats(self):
+        print(f"stats of: {self.name}")
+        print(f"Name: {self.name}")
         print(f"Armor: {self.armor}, HP: {self.hp}, Force: {self.force}, Dexterity: {self.dexterity}")
         print(f"Constitution: {self.constitution}, Intelligence: {self.intelligence}, Charisma: {self.charisma}")
-        print(f"Sagesse: {self.sagesse}")
+        print(f"Sagesse: {self.sagesse}\n")
+
+    def attaque(self, target):
+        self.damage = dice(6)
+        target.hp -= self.damage
+        print(f"{self.name} hit {target.name} for {self.damage}!")
+
+
+    def talk(self, message):
+        print(f"{self.name}: {message}\n")
+
+class kobold(NPC):
+    pass
+
+class hero(NPC):
+    pass
+
+jeff = hero("Jeff")
+jeff.stats()
+kobold = kobold("Kobold the 1.294037 * 10^8 th")
+kobold.stats()
+jeff.attaque(kobold)
+kobold.talk("ow")
+kobold.stats()
 
