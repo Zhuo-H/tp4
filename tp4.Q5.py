4ba1138353bef9687ed3fae214d32a2139bff2ca
hanz <hanz@educ.college-montreal.qc.ca>
2026-01-14 14:49:56 -0500

    stuff

--- tp4.Q5.py
+++ tp4.Q5.py
@@ -1,13 +1,23 @@
 import random
 from enum import Enum
 
+
 class Allignement(Enum):
     LAWFUL_GOOD = 0
     NEUTRAL_GOOD= 1
+    CHAOTIC_GOOD = 2
+    LAWFUL_NEUTRTAL = 3
+    TRUE_NEUTRAL = 4
+    CHAOTIC_NEUTRAL = 5
+    LAWFUL_EVIL = 6
+    NEUTRAL_EVIL = 7
+    CHAOTIC_EVIL = 8
+
 
 def dice(y):
     result = random.randint(1, y)
     return result
+
 
 def picker():
     rolls = [dice(6) for z in range(4)]
@@ -16,7 +26,7 @@ def picker():
 
 
 class NPC:
-    def __init__(self, name):
+    def __init__(self, name, karma):
         self.armor = dice(12)
         self.hp = dice(20)
         self.force = picker()
@@ -26,6 +36,7 @@ class NPC:
         self.charisma = picker()
         self.sagesse = picker()
         self.name = name
+        self.karma = karma
 
     def stats(self):
         print(f"stats of: {self.name}")
@@ -46,25 +57,32 @@ class NPC:
             target.hp -= damage
             print(f"{self.name} hit {target.name} for {damage}!\n")
 
-
     def talk(self, message):
         print(f"{self.name}: {message}\n")
 
+    def check_alive(self):
+        if self.hp > 0:
+            pass
+        elif self.hp <= 0:
+            print(f"{self.name} is dead")
+
+
 class Kobold(NPC):
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, name, karma):
+        super().__init__(name, karma)
 
 
 class Hero(NPC):
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, name, karma):
+        super().__init__(name, karma)
 
 
-jeff = Hero("Jeff")
+jeff = Hero("Jeff", Allignement.LAWFUL_GOOD)
 jeff.stats()
-kobold = Kobold("Kobold")
+kobold = Kobold("Kobold", Allignement.CHAOTIC_EVIL)
 kobold.stats()
 jeff.attaque(kobold)
 kobold.talk("ow")
 kobold.stats()
+kobold.check_alive()
 
